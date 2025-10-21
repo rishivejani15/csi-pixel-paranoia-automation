b786428caf20f1ce457b17e4da9ee932b98b8d7b
Rishi Vejani <123580547+rishivejani15@users.noreply.github.com>
2025-10-21 23:29:35 +0530

    verify,py

--- verify.py
+++ verify.py
@@ -4,12 +4,12 @@ from supabase import create_client
 from dotenv import load_dotenv
 import os
 
+app = FastAPI()
+
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
-app = FastAPI()
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
